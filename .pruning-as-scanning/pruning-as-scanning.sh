ca55a13cac9ec869feb18dad1cbf6f7c1dff2069
latteyt <lattetao@outlook.com>
2026-04-20 09:29:13 +0800

    优化pruning-as-scanning.sh脚本

--- .pruning-as-scanning/pruning-as-scanning.sh
+++ .pruning-as-scanning/pruning-as-scanning.sh
@@ -1,3 +1,11 @@
+
+check_file_exists() {
+  if [[ ! -f "$1" ]]; then
+    echo "Error: $1 not found" >&2
+    exit 1
+  fi
+}
+
 filter() {
   case $1 in
     32) str_len=9  ; suffix="::/32" ;;
@@ -14,19 +22,15 @@ filter() {
   }'
 }
 
-check_file_exists() {
-  if [[ ! -f "$1" ]]; then
-    echo "Error: $1 not found" >&2
-    exit 1
-  fi
-}
-
-# 生成 .ini 文件模板
 generate_ini_file() {
-  local limit=$1
-  local input=$2
+  case $1 in
+    32) input="IANA.txt"; repeat=512 ;;
+    48) input=".pruning-as-scanning/prefix32.txt" ; repeat=64 ;;
+    56) input=".pruning-as-scanning/prefix48.txt" ; repeat=16 ;;
+    64) input=".pruning-as-scanning/prefix56.txt" ; repeat=1 ;;
+  esac
 
-  cat <<EOF > ".pruning-as-scanning/scan${limit}.ini"
+  cat <<EOF > ".pruning-as-scanning/scan$1.ini"
 [Net]
 L3Src   = $L3_SRC
 L2Dst   = $L2_DST
@@ -35,10 +39,10 @@ IF      = $IF_NAME
 [Runtime]
 shard   = 2
 rate    = 100000
-repeat  = 512
+repeat  = $repeat
 
 seed    = 521
-limit   = $limit
+limit   = $1
 
 [Scan]
 type    = net
@@ -55,15 +59,13 @@ L3_SRC=$(ip -6 addr show dev "$IF_NAME" | grep "inet6" | grep "global" |  awk '!
 L2_DST=$(ip -6 neigh show dev "$IF_NAME" | grep "router" | awk '!seen[$3]++{print $3}')
 
 limits=(32 48 56 64)
-inputs=("IANA.txt" ".pruning-as-scanning/prefix32.txt" ".pruning-as-scanning/prefix48.txt" ".pruning-as-scanning/prefix56.txt")
 
 # Ensure IANA.txt exists
 check_file_exists "IANA.txt"
 
 for i in "${!limits[@]}"; do
   limit="${limits[$i]}"
-  input="${inputs[$i]}"
-  generate_ini_file "$limit" "$input"
+  generate_ini_file "$limit"
   sudo ./build/ymap ".pruning-as-scanning/scan${limit}.ini" > ".pruning-as-scanning/scan${limit}.txt"
   check_file_exists ".pruning-as-scanning/scan${limit}.txt"
   if [[ $limit != 64 ]]; then
